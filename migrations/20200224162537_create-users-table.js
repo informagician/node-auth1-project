8658859afe7ec7581b1719843c623c2de08ae8a2
Milo Rastgoo <hi@informagician.com>
2020-02-24 16:39:11 -0500

    migration file created - prior to migrate latest

--- migrations/20200224162537_create-users-table.js
+++ migrations/20200224162537_create-users-table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+  knex.schema.createTable('users', tbl => {
+      tbl.increments()
+      tbl.string('username',32).unique().notNullable()
+      tbl.string('password',64).notNullable()
+  })
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTableIfExists('users')
 };
