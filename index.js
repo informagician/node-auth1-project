6653fb3e72381276cc0ddaf96046326c74d4a811
Milo Rastgoo <hi@informagician.com>
2020-02-24 16:26:17 -0500

    endpoint references + database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const express = require('express')
 
 const server = express()
@@ -8,5 +9,20 @@ server.get('/', (req,res) => {
     res.status(200).json({ message: 'API is working!'})
 })
 
+server.post('/api/register', (req,res) => {
+    const user = req.body
+    user.password = bcrypt.hashSync(user.password,12)
+    
+})
+
+server.post('/api/login', (req,res) => {
+    const user = req.body
+
+})
+
+server.get('/api/users', (req,res) => {
+    
+
+})
 
 server.listen(5000, () => console.log('Server is on 5000'))
